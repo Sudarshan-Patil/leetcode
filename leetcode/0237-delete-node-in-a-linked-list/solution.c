e651985fa505ecbf7e2aa18070a9a0458ccb9caf
Sudarshansp <sudarshansp123@gmail.com>
2020-10-13 19:25:45 +0000

    [LeetCode Sync] Runtime - 4 ms (60.52%), Memory - 6.4 MB (8.26%)

---
+++ leetcode/0237-delete-node-in-a-linked-list/solution.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    struct ListNode* temp;
+    temp = node->next;
+    node->val = node->next->val;
+    node->next = node->next->next;
+    free(temp);
+}

1a7bed5294252441ee457fec16c3ca16619632fe
Sudarshansp <sudarshansp123@gmail.com>
2020-10-13 20:23:58 +0000

    [LeetCode Sync] Runtime - 8 ms (51.60%), Memory - 7.5 MB (99.85%)

---
+++ leetcode/0141-linked-list-cycle/solution.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    struct ListNode* slow;
+    struct ListNode* fast;
+    
+    slow = fast = head;
+    
+    while (fast!=NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}

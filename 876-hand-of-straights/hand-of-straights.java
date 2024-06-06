1f7e6890816e756ea9484131b007b73fb8a8a997
Sudarshansp <sudarshansp123@gmail.com>
2024-06-06 17:54:07 +0530

    Time: 8 ms (100.00%) | Memory: 44.8 MB (93.23%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -3,36 +3,36 @@ class Solution {
         int n = hand.length;
         if (n % groupSize != 0)
             return false;
+
         Arrays.sort(hand);
-        HashMap<Integer, Integer> hm = new HashMap<>();
 
         for (int i=0; i<hand.length; i++) {
-            if (hm.containsKey(hand[i])) {
-                hm.put(hand[i], hm.get(hand[i])+1);
-            } else {
-                hm.put(hand[i], 1);
-            }
-        }
-
-        for (int i=0; i<hand.length; i++) {
-            if (hm.containsKey(hand[i])) {
-                int cnt = 0;
-                int t = hand[i];
-                while (cnt < groupSize && hm.containsKey(t)) {
-                    hm.put(t, hm.get(t)-1);
-                    if (hm.get(t) == 0) {
-                        hm.remove(t);
-                    }
-                    t = t+1;
-                    cnt++;
-                }
-
-                if (cnt < groupSize) {
+            if (hand[i] >= 0) {
+                if (!valid(hand, i, groupSize)) {
                     return false;
                 }
             }
         }
 
         return true;
+    }
+
+    public boolean valid(int[] hand, int i, int g) {
+        int cnt = 1;
+        int temp = hand[i]+1;
+        while (cnt < g && i < hand.length) {
+            if (hand[i] == temp) {
+                temp = hand[i]+1;
+                hand[i] = -1;
+                cnt++;
+            }
+            i++;
+        }
+
+        if (cnt == g) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }

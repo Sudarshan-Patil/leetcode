1dc6f70a34e7b01d5ad772094805f690df3bfafb
Sudarshansp <sudarshansp123@gmail.com>
2024-06-21 14:15:30 +0530

    Time: 3 ms (91.49%) | Memory: 47.6 MB (16.32%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -2,16 +2,14 @@ class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         int ans = 0;
         for (int i=0; i<customers.length; i++) {
-            if (grumpy[i] == 0) {
+            if (i < minutes) {
                 ans += customers[i];
+            } else {
+                ans += customers[i] * (1-grumpy[i]);
             }
+            
         }
 
-        for (int i=0; i<minutes; i++) {
-            if (grumpy[i] == 1) {
-                ans += customers[i];
-            }
-        }
         int res = ans;
 
         for (int i=minutes; i<customers.length; i++) {

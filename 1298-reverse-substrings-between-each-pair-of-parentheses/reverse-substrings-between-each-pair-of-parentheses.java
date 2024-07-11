0027ebc96b80fb23cb01707f7bdc22aa12d948b2
Sudarshansp <sudarshansp123@gmail.com>
2024-07-11 21:40:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (97.95%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,31 +1,27 @@
 class Solution {
+    int i=0;
     public String reverseParentheses(String s) {
-        Stack<Character> st = new Stack<>();
-        for (int i=0; i<s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                st.push(s.charAt(i));
-            } else if (s.charAt(i) == ')') {
-                StringBuilder sb = new StringBuilder();
-                while (!st.isEmpty() && st.peek()!='(') {
-                    sb.append(st.pop());
-                }
-                st.pop();
-                for (int j=0; j<sb.length(); j++) {
-                    st.push(sb.charAt(j));
-                }
+        return solve(s);
+    }
+
+    public String solve(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        while (i<s.length()) {
+            if (s.charAt(i) == ')') {
+                i++;
+                return sb.reverse().toString();
+            } else if (s.charAt(i) == '(') {
+                i++;
+                String str = solve(s);
+
+                sb.append(str);
             } else {
-                st.push(s.charAt(i));
+                sb.append(s.charAt(i));
+                i++;
             }
         }
 
-        if (!st.isEmpty()) {
-            StringBuilder sb = new StringBuilder();
-            while (!st.isEmpty()) {
-                sb.append(st.pop());
-            }
-
-            return sb.reverse().toString();
-        }
-        return "";
+        return sb.toString();
     }
 }

3f5d28f663880a89bcef61eb1ea4f9503b177989
Sudarshansp <sudarshansp123@gmail.com>
2024-07-21 00:40:38 +0530

    Time: 1 ms (100.00%) | Memory: 53 MB (41.43%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -1,28 +1,34 @@
 class Solution {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        int n = rowSum.length;
-        int m = colSum.length;
+        int rows = rowSum.length;
+        int cols = colSum.length;
+        int cur_row = 0, cur_col = 0;
+        int[][] res = new int[rows][cols];
 
-        int[][] ans = new int[n][m];
+        while (cur_row < rows && cur_col < cols) {
+            // if (cur_row >= rows) {
+            //     res[rows - 1][cur_col] = colSum[cur_col];
+            //     cur_col++;
+            //     continue;
+            // } else if (cur_col >= cols) {
+            //     res[cur_row][cols - 1] = rowSum[cur_row];
+            //     cur_row++;
+            //     continue;
+            // }
 
-        int i=0,j=0;
+            int value_to_put = Math.min(rowSum[cur_row], colSum[cur_col]);
+            rowSum[cur_row] -= value_to_put;
+            colSum[cur_col] -= value_to_put;
+            res[cur_row][cur_col] = value_to_put;
 
-        while (i<n && j<m) {
-            int val = Math.min(rowSum[i], colSum[j]);
-
-            ans[i][j] = val;
-
-            rowSum[i] -= val;
-            colSum[j] -= val;
-
-            if (rowSum[i] == 0) {
-                i++;
+            // I write this as this because it's possible that rowSum[cur_row] == colSum[cur_col] and we'll want to move both row and col pointers
+            if (rowSum[cur_row] == 0) {
+                cur_row++;
             }
-            if (colSum[j] == 0) {
-                j++;
+            if (colSum[cur_col] == 0) {
+                cur_col++;
             }
         }
-
-        return ans;
+        return res;
     }
 }

df829e3700ae87cafed41dd551eca9457f5e2a21
Sudarshansp <sudarshansp123@gmail.com>
2024-08-06 23:38:12 +0530

    Time: 2 ms (97.43%) | Memory: 53.8 MB (39.16%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,15 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        HashSet<Integer> hs = new HashSet<>();
-        int[] arr = new int[n+1];
+        int[] arr1 = new int[n+1];
+        int[] arr2 = new int[n+1];
 
         for (int i=0; i<trust.length; i++) {
-            arr[trust[i][1]]++;
-            hs.add(trust[i][0]);
+            arr1[trust[i][0]]++;
+            arr2[trust[i][1]]++;
         }
         
         for (int i=1; i<=n; i++) {
-            if (arr[i] == n-1 && !hs.contains(i)) {
+            if (arr2[i] == n-1 && arr1[i] == 0) {
                 return i;
             }
         }

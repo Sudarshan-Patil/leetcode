413eb75cfed8efd54ae471eb94c2ca710700328d
Sudarshansp <sudarshansp123@gmail.com>
2024-10-07 22:13:10 +0530

    Time: 4 ms (85.00%) | Memory: 43.2 MB (60.85%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == 'B') {
+                if (!st.isEmpty() && st.peek() == 'A') {
+                    st.pop();
+                } else {
+                    st.push(s.charAt(i));
+                }
+            } else if (s.charAt(i) == 'D') {
+                if (!st.isEmpty() && st.peek() == 'C') {
+                    st.pop();
+                } else {
+                    st.push(s.charAt(i));
+                }
+            } else {
+                st.push(s.charAt(i));
+            }
+        }
+        
+        return st.size();
+    }
+}

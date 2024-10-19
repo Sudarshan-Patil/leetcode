a2cfbba24f9bc635bdc4c8f95778a6e58da86f62
Sudarshansp <sudarshansp123@gmail.com>
2024-10-19 16:49:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (85.41%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,39 +1,21 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        String s = "0";
-
-        while (n > 1) {
-            s = s + "1" + reverse(s);
-            n--;
-        }
-
-        return s.charAt(k-1);
-    }
-
-    public String reverse(String s) {
-        StringBuilder str = invert(s);
-        int i=0;
-        int j=s.length()-1;
-
-        while (i<j) {
-            char ch = str.charAt(i);
-            str.setCharAt(i, str.charAt(j));
-            str.setCharAt(j, ch);
-            i++;
-            j--;
-        }
-
-        return str.toString();
-    }
-
-    public StringBuilder invert(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        for (int i=0; i<s.length(); i++) {
-            char ch = sb.charAt(i) == '0' ? '1' : '0';
-            sb.setCharAt(i, ch);
-        }
+        // Base case: When n = 1, the binary string is "0"
+        if (n == 1) return '0';
         
-
-        return sb;
+        // Find the length of the current string Sn, which is 2^n - 1
+        int length = (1 << n) - 1;
+        
+        // Find the middle position
+        int mid = length / 2 + 1;
+        
+        // If k is the middle position, return '1'
+        if (k == mid) return '1';
+        
+        // If k is in the first half, find the bit in Sn-1
+        if (k < mid) return findKthBit(n - 1, k);
+        
+        // If k is in the second half, find the bit in Sn-1 and invert it
+        return findKthBit(n - 1, length - k + 1) == '0' ? '1' : '0';
     }
 }

8270167d036f2bf463ef6317ab7ffddc0bf6840f
Sudarshansp <sudarshansp123@gmail.com>
2024-10-22 17:55:03 +0530

    Time: 11 ms (99.72%) | Memory: 80 MB (5.93%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
@@ -14,38 +14,60 @@
  * }
  */
 class Solution {
-    public long kthLargestLevelSum(TreeNode root, int k) {
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        PriorityQueue<Long> pq = new PriorityQueue<Long>();
+    private int exploreLevels(TreeNode root, int level, long[] level_to_sum) {
+        level_to_sum[level] += root.val;
+        int left_level = level;
+        if (root.left != null) {
+            left_level = exploreLevels(root.left, level + 1, level_to_sum);
+        }
+        int right_level = level;
+        if (root.right != null) {
+            right_level = exploreLevels(root.right, level + 1, level_to_sum);
+        }
+        return Math.max(left_level, right_level);
+    }
 
-        q.offer(root);
-        q.offer(null);
-        long sum = 0;
-        while (!q.isEmpty()) {
-            TreeNode t = q.poll();
+    private void swap(long[] values, int left, int right) {
+        long temp = values[left];
+        values[left] = values[right];
+        values[right] = temp;
+    }
 
-            if (t == null) {
-                pq.offer(sum);
-                if (pq.size() > k) {
-                    pq.poll();
-                }
-                sum = 0;
-                if (!q.isEmpty()) {
-                    q.offer(null);
-                }
-            } else {
-                sum += t.val;
-
-                if (t.left!=null) {
-                    q.offer(t.left);
-                }
-
-                if (t.right!=null) {
-                    q.offer(t.right);
-                }
+    private int partition(long[] values, int start, int end, int target) {
+        int current = start;
+        long pivot_value = values[start];
+        for (int i = current + 1; i <= end; i++) {
+            long value = values[i];
+            if (value <= pivot_value) {
+                swap(values, current + 1, i);
+                current++;
             }
         }
+        swap(values, start, current);
+        return current;
+    }
 
-        return pq.size() == k ? pq.peek() : -1;
+    private long largest(long[] values, int start, int end, int target) {
+        int index = partition(values, start, end, target);
+        if (index < target) {
+            return largest(values, index + 1, end, target);
+        } else if (index == target) {
+            return values[index];
+        } else {
+            long pivot_value = values[index];
+            while (index > target && values[index] == pivot_value) {
+                index--;
+            }
+            return largest(values, start, index, target);
+        }
+    }
+
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        long[] level_to_sum = new long[100001];
+        int max_level = exploreLevels(root, 0, level_to_sum);
+        if (k > (max_level + 1)) {
+            return -1;
+        }
+        return largest(level_to_sum, 0, max_level, max_level + 1 - k);
     }
 }

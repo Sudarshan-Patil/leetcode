65eab6e9bea50c2f64a97d5165c2a934d8fa683c
Sudarshansp <sudarshansp123@gmail.com>
2024-11-08 01:50:25 +0530

    Time: 1 ms (75.58%) | Memory: 41.5 MB (45.97%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.java
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -1,53 +1,53 @@
 class Solution {
-    public int minMutation(String startGene, String endGene, String[] bank) {
-        Queue<String> q = new LinkedList<String>();
-        Queue<Integer> tracker = new LinkedList<Integer>();
-        HashMap<String, Integer> hm = new HashMap<>();
+public int minMutation(String startGene, String endGene, String[] bank) {
+	HashMap<String, Integer> hm = new HashMap<>();
+	for (int i=0; i<bank.length; i++) {
+		if (!hm.containsKey(bank[i])) {
+			hm.put(bank[i], 1);
+} else {
+	hm.put(bank[i], hm.get(bank[i])+1);
+}
+}
 
-        for (int i=0; i<bank.length; i++) {
-            if (!hm.containsKey(bank[i])) {
-                hm.put(bank[i], 1);
-            } else {
-                hm.put(bank[i], hm.get(bank[i])+1);
-            }
-        }
+		Queue<String> q = new LinkedList<>();
+		Queue<Integer> counter = new LinkedList<>();
 
-        tracker.offer(0);
-        q.offer(startGene);
-        while(!q.isEmpty()) {
-            String s = q.poll();
-            int count = tracker.poll();
+		q.offer(startGene);
+		counter.offer(0);
 
-            if (s.equals(endGene)) {
-                return count;
-            }
+		while (!q.isEmpty()) {
+			String s = q.poll();
+int count = counter.poll();
 
-            for (String key:hm.keySet()) {
-                if (mutate(s, key) && hm.get(key) > 0) {
-                    q.offer(key);
-                    tracker.offer(count+1);
-                    hm.put(key, hm.get(key)-1);
-                }
-            }
-        }
+if (s.equals(endGene)) {
+	return count;
+}
 
-        return -1;
-    }
+for (String key:hm.keySet()) {
+	if (mutate(s, key) && hm.get(key) > 0) {
+	q.offer(key);
+	counter.offer(count+1);
+	hm.put(key, hm.get(key)-1);
+}
+}
+}
 
-    public boolean mutate(String s1, String s2) {
-        int n = s1.length();
-        int count = 0;
-        for (int i=0; i<n; i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                count++;
-            }
+return -1;
+}
 
-            if (count > 1) {
-                return false;
-            }
-        }
+public boolean mutate(String s1, String s2) {
+	int i=0;
+	int countlen = 0;
+	while (i<s1.length()) {
+		if (s1.charAt(i) != s2.charAt(i)) {
+			countlen++;
+}
 
-        return true;
-    }
-
+if (countlen > 1) {
+	return false;
+}
+i++;
+}
+return true;
+}
 }

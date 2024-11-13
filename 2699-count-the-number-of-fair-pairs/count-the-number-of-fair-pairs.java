64a799e956e64521ecfe69ff9fd7fb4cebd95499
Sudarshansp <sudarshansp123@gmail.com>
2024-11-13 12:05:53 +0530

    Time: 23 ms (100.00%) | Memory: 57.8 MB (5.76%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,43 +1,27 @@
 class Solution {
-
-    long lower_bound(int[] nums, int low, int high, int element) {
-        while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            if (nums[mid] >= element) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums); 
+        long pairsWithUpper = findLessThanEqualTo(nums, upper);
+        long pairsWithLowerMinus1 = findLessThanEqualTo(nums, lower - 1);
+        return pairsWithUpper - pairsWithLowerMinus1;
     }
 
-    long countFairPairs(int[] nums, int lower, int upper) {
-        Arrays.sort(nums);
-        long ans = 0;
-        for (int i = 0; i < nums.length; i++) {
-            // Assume we have picked nums[i] as the first pair element.
+    private long findLessThanEqualTo(int[] nums, int targetSum) {
+        int n = nums.length;
+        long pairs = 0;
+        int left = 0;
+        int right = n - 1;
 
-            // `low` indicates the number of possible pairs with sum < lower.
-            long low = lower_bound(
-                nums,
-                i + 1,
-                nums.length - 1,
-                lower - nums[i]
-            );
 
-            // `high` indicates the number of possible pairs with sum <= upper.
-            long high = lower_bound(
-                nums,
-                i + 1,
-                nums.length - 1,
-                upper - nums[i] + 1
-            );
-
-            // Their difference gives the number of elements with sum in the
-            // given range.
-            ans += 1 * (high - low);
+        while (left < right) {
+            if (nums[left] + nums[right] <= targetSum) {
+                pairs += (right - left);
+                left++; 
+            } else {
+                right--; 
+            }
         }
-        return ans;
+
+        return pairs;
     }
 }

9793151110b38819fda5db3c5c17aedcbd1c9d21
Sudarshansp <sudarshansp123@gmail.com>
2024-11-17 20:36:21 +0530

    Time: 1 ms (77.36%) | Memory: 41.3 MB (72.51%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.java
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -1,53 +1,40 @@
 class Solution {
-public int minMutation(String startGene, String endGene, String[] bank) {
-	HashMap<String, Integer> hm = new HashMap<>();
-	for (int i=0; i<bank.length; i++) {
-		if (!hm.containsKey(bank[i])) {
-			hm.put(bank[i], 1);
-} else {
-	hm.put(bank[i], hm.get(bank[i])+1);
-}
-}
+    public int minMutation(String beginWord, String endWord, String[] wordList) {
+        HashSet<String> hs = new HashSet<>(Arrays.asList(wordList));
+        HashSet<Character> hsChar = new HashSet<>();
+        hsChar.add('A');
+        hsChar.add('C');
+        hsChar.add('G');
+        hsChar.add('T');
 
-		Queue<String> q = new LinkedList<>();
-		Queue<Integer> counter = new LinkedList<>();
+        Queue<String> q = new LinkedList<String>();
+        q.offer(beginWord);
+        int ans = 0;
 
-		q.offer(startGene);
-		counter.offer(0);
+        while (!q.isEmpty()) {
+            int n = q.size();
+            for (int i=0; i<n; i++) {
+                StringBuilder s = new StringBuilder(q.poll());
+                for (int j=0; j<s.length(); j++) {
+                    char old = s.charAt(j);
+                    for (char t:hsChar) {
+                        s.setCharAt(j, t);
+                        String temp = s.toString();
+                        if (hs.contains(temp)) {
+                            if (temp.equals(endWord)) {
+                                return ans + 1;
+                            }
+                            hs.remove(temp);
+                            q.offer(temp);
+                        }
+                    }
+                    s.setCharAt(j, old);
+                }
+            }
+            ans++;
+        }
 
-		while (!q.isEmpty()) {
-			String s = q.poll();
-int count = counter.poll();
 
-if (s.equals(endGene)) {
-	return count;
-}
-
-for (String key:hm.keySet()) {
-	if (mutate(s, key) && hm.get(key) > 0) {
-	q.offer(key);
-	counter.offer(count+1);
-	hm.put(key, hm.get(key)-1);
-}
-}
-}
-
-return -1;
-}
-
-public boolean mutate(String s1, String s2) {
-	int i=0;
-	int countlen = 0;
-	while (i<s1.length()) {
-		if (s1.charAt(i) != s2.charAt(i)) {
-			countlen++;
-}
-
-if (countlen > 1) {
-	return false;
-}
-i++;
-}
-return true;
-}
+        return -1;
+    }
 }

92e51a82313fd498981c62e32dda6a96779f6a68
Sudarshansp <sudarshansp123@gmail.com>
2024-11-29 21:41:21 +0530

    Time: 1 ms (32.20%) | Memory: 41.4 MB (32.45%) - LeetSync

--- 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.java
+++ 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.java
@@ -1,10 +1,24 @@
 class Solution {
     public boolean queryString(String s, int n) {
         for(int i=1;i<=n;i++){
-            if(!s.contains(Integer.toBinaryString(i))){
+            if(!s.contains(getBinary(i))){
                 return false;
             }
         }
         return true;
+    }
+
+    public String getBinary(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n!=0) {
+            if ((n&1) == 1) {
+                sb.append(1);
+            } else {
+                sb.append(0);
+            }
+            n = n>>1;
+        }
+
+        return sb.reverse().toString();
     }
 }

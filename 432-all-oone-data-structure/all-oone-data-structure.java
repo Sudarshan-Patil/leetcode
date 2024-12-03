b2c6659becb2f2102b30433bbc340bfa126c9ce3
Sudarshansp <sudarshansp123@gmail.com>
2024-12-03 10:36:53 +0530

    Time: 75 ms (93.72%) | Memory: 64.6 MB (37.76%) - LeetSync

--- 432-all-oone-data-structure/all-oone-data-structure.java
+++ 432-all-oone-data-structure/all-oone-data-structure.java
@@ -1,34 +1,119 @@
-class AllOne {
-    private Map<String, Integer> count;
-    private TreeSet<Pair<Integer, String>> set;
+public class Node {
 
-    public AllOne() {
-        count = new HashMap<>();
-        set = new TreeSet<>((a, b) -> a.getKey().equals(b.getKey()) ? a.getValue().compareTo(b.getValue()) : a.getKey() - b.getKey());
+    int freq;
+    Node prev;
+    Node next;
+    Set<String> keys = new HashSet<>();
+
+    Node(int freq) {
+        this.freq = freq;
+    }
+}
+
+class AllOne {
+
+    Node head;
+    Node tail;
+    Map<String, Node> map = new HashMap<>();
+
+    AllOne() {
+        head = new Node(0);
+        tail = new Node(0);
+        head.next = tail;
+        tail.prev = head;
     }
 
     public void inc(String key) {
-        int n = count.getOrDefault(key, 0);
-        count.put(key, n + 1);
-        set.remove(new Pair<>(n, key));
-        set.add(new Pair<>(n + 1, key));
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            int freq = node.freq;
+            node.keys.remove(key);
+
+            Node nextNode = node.next;
+            if (nextNode == tail || nextNode.freq != freq + 1) {
+                Node newNode = new Node(freq + 1);
+                newNode.keys.add(key);
+                newNode.prev = node;
+                newNode.next = nextNode;
+                node.next = newNode;
+                nextNode.prev = newNode;
+                map.put(key, newNode);
+            } else {
+                nextNode.keys.add(key);
+                map.put(key, nextNode);
+            }
+
+            if (node.keys.isEmpty()) {
+                removeNode(node);
+            }
+        } else {
+            Node firstNode = head.next;
+            if (firstNode == tail || firstNode.freq > 1) {
+                Node newNode = new Node(1);
+                newNode.keys.add(key);
+                newNode.prev = head;
+                newNode.next = firstNode;
+                head.next = newNode;
+                firstNode.prev = newNode;
+                map.put(key, newNode);
+            } else {
+                firstNode.keys.add(key);
+                map.put(key, firstNode);
+            }
+        }
     }
 
     public void dec(String key) {
-        int n = count.get(key);
-        set.remove(new Pair<>(n, key));
-        if (n == 1) count.remove(key);
-        else {
-            count.put(key, n - 1);
-            set.add(new Pair<>(n - 1, key));
+        if (!map.containsKey(key)) {
+            return;
+        }
+
+        Node node = map.get(key);
+        node.keys.remove(key);
+        int freq = node.freq;
+
+        if (freq == 1) {
+            map.remove(key);
+        } else {
+            Node prevNode = node.prev;
+            if (prevNode == head || prevNode.freq != freq - 1) {
+                Node newNode = new Node(freq - 1);
+                newNode.keys.add(key);
+                newNode.prev = prevNode;
+                newNode.next = node;
+                prevNode.next = newNode;
+                node.prev = newNode;
+                map.put(key, newNode);
+            } else {
+                prevNode.keys.add(key);
+                map.put(key, prevNode);
+            }
+        }
+
+        if (node.keys.isEmpty()) {
+            removeNode(node);
         }
     }
 
     public String getMaxKey() {
-        return set.isEmpty() ? "" : set.last().getValue();
+        if (tail.prev == head) {
+            return "";
+        }
+        return tail.prev.keys.iterator().next();
     }
 
     public String getMinKey() {
-        return set.isEmpty() ? "" : set.first().getValue();
+        if (head.next == tail) {
+            return "";
+        }
+        return head.next.keys.iterator().next();
+    }
+
+    private void removeNode(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
     }
 }

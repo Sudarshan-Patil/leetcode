d43bfe889067ae481dfb41e57bd58d3931b2ccf0
Sudarshansp <sudarshansp123@gmail.com>
2024-12-05 15:59:22 +0530

    Time: 16 ms (50.00%) | Memory: 45.9 MB (23.44%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        int i=0;
+        int j=0;
+
+        while (i<start.length() || j<target.length()) {
+            while (i<start.length() && start.charAt(i) == '_') {
+                i++;
+            }
+
+            while (j<target.length() && target.charAt(j) == '_') {
+                j++;
+            }
+            
+            if (i==start.length() || j==target.length()) {
+                return i == start.length() && j == target.length();
+            }
+
+            if (
+                start.charAt(i) != target.charAt(j) ||
+                (start.charAt(i) == 'L' && i<j) ||
+                (start.charAt(i) == 'R' && i>j)
+            ) {
+                return false;        
+            }
+            i++;
+            j++;
+        }
+
+        return true;
+    }
+}

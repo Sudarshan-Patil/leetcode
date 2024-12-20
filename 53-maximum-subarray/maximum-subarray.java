cd4e92ce5512e0436649a405fb72b32b1fe6428e
Sudarshansp <sudarshansp123@gmail.com>
2024-12-21 00:03:30 +0530

    Time: 1 ms (99.39%) | Memory: 57 MB (60.94%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -6,9 +6,7 @@ class Solution {
         for (int i=0; i<nums.length; i++) {
             sum += nums[i];
             max = Math.max(sum, max);
-            if (sum < 0) {
-                sum = 0;
-            }
+            sum = Math.max(sum, 0);
         }
 
         return max;

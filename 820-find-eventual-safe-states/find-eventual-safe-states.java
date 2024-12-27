8eb56f4ea14cc28c1578b88d70414fd974fcbb4f
Sudarshansp <sudarshansp123@gmail.com>
2024-12-27 14:48:37 +0530

    Time: 50 ms (7.90%) | Memory: 55.3 MB (73.59%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,49 +1,48 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int size = graph.length;
+        int[] indegree = new int[size];
+        Queue<Integer> q = new LinkedList<>();
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        List<Integer> ans = new ArrayList<Integer>();
 
-        int[] vis = new int[size];
-        int[] pathvis = new int[size];
-
-        for (int i=0; i<size; i++) {
-            if (vis[i] == 0) {
-                dfs(i, graph, vis, pathvis);
-            }
-        }
-
-        List<Integer> ans = new ArrayList<>();
-
-        for (int i=0; i<size; i++) {
-            if (vis[i] == 1 && pathvis[i] == 0) {
-                ans.add(i);
-            }
-        }
-
-        return ans;
-    }
-
-    public boolean dfs(int index, int[][] graph, int[] vis, int[] pathvis) {
-        vis[index] = 1;
-        pathvis[index] = 1;
-        for (int i=0; i<graph[index].length; i++) {
-            int start = graph[index][i];
-            if (vis[start] == 0) {
-                if (dfs(start, graph, vis, pathvis)) {
-                    return true;
+        for (int i=0; i<graph.length; i++) {
+            int end = i;
+            for (int j=0; j<graph[i].length; j++) {
+                int start = graph[i][j];
+                if (!hm.containsKey(start)) {
+                    ArrayList<Integer> arr = new ArrayList<>();
+                    arr.add(end);
+                    hm.put(start, arr);
+                } else {
+                    hm.get(start).add(end);
                 }
-            } else if (pathvis[start] == 1) {
-                return true;
+            }
+            indegree[i] = graph[i].length;
+        }
+
+        for (int i=0; i<size; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
             }
         }
-        pathvis[index] = 0;
 
-        return false;
-    }
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            ans.add(temp);
 
-    public void display(int[] arr) {
-        for (int i=0; i<arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            if (!hm.containsKey(temp)) {
+                continue;
+            }
+
+            for (int i=0; i<hm.get(temp).size(); i++) {
+                indegree[hm.get(temp).get(i)]--;
+                if (indegree[hm.get(temp).get(i)] == 0) {
+                    q.offer(hm.get(temp).get(i));
+                }
+            }
         }
-        System.out.println();
+        Collections.sort(ans);
+        return ans;
     }
 }

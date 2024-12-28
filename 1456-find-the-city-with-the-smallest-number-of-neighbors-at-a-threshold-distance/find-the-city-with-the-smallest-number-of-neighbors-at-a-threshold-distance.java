8499a738fbebdc7f4699922e62ad953b00033937
Sudarshansp <sudarshansp123@gmail.com>
2024-12-29 03:15:57 +0530

    Time: 9 ms (75.26%) | Memory: 44.5 MB (55.79%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,50 +1,62 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        // first convert graph to adjacency list representation
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] edge : edges) {
-            int node1 = edge[0], node2 = edge[1], distance = edge[2];
-            graph.get(node1).add(new int[]{node2, distance});
-            graph.get(node2).add(new int[]{node1, distance});
-        }
-
-        int minimum_number = n;
-        int res = -1;
-
-        for (int source = 0; source < n; source++) {
-            int neighbors = get_number_of_neighbors_in_distance(graph, source, n, distanceThreshold);
-            // we iterate source from smaller to bigger this ensures that we choose node with greater value if they have equal number of neighbors
-            if (neighbors <= minimum_number) {
-                minimum_number = neighbors;
-                res = source;
+        int[][] matrix = new int[n][n];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (i!=j) {
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
             }
         }
 
-        return res;
-    }
+        for (int i=0; i<edges.length; i++) {
+            int start = edges[i][0];
+            int end = edges[i][1];
+            int distance = edges[i][2];
+            matrix[start][end] = distance;
+            matrix[end][start] = distance;
+        }
 
-    private int get_number_of_neighbors_in_distance(List<List<int[]>> graph, int source, int n, int distanceThreshold) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        minHeap.add(new int[]{0, source}); // distance to node itself is 0
-        Set<Integer> visited = new HashSet<>();
-
-        while (!minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int distance_to_this_node = top[0], cur_node = top[1];
-            if (!visited.contains(cur_node)) {
-                visited.add(cur_node);
-                for (int[] neighbor : graph.get(cur_node)) {
-                    int distance_from_source = distance_to_this_node + neighbor[1];
-                    if (distance_from_source <= distanceThreshold) { // ensure that we're allowed to go to this node
-                        minHeap.add(new int[]{distance_from_source, neighbor[0]});
+        for (int k=0; k<n; k++) {
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<n; j++) {
+                    if (matrix[i][k] != Integer.MAX_VALUE && matrix[k][j] != Integer.MAX_VALUE) {
+                        matrix[i][j] = Math.min(
+                            matrix[i][j],
+                            matrix[i][k] + matrix[k][j]
+                        );
                     }
                 }
             }
         }
-        // actually you can return visited.size() and with math there will be nothing wrong but actually we have visited.size() - 1 neighbors since we're not neighbor of ourselves
-        return visited.size() - 1;
+
+        
+
+        int ans = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<n; i++) {
+            int count = 0;
+            for (int j=0; j<n; j++) {
+                if (matrix[i][j] <= distanceThreshold) {
+                    count++;
+                }
+            }
+            if (count <= min) {
+                min = count;
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+
+    public void display(int[][] matrix, int n) {
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return;
     }
 }

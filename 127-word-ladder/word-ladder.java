df0407b65f85892af0b9ee68bb9d933ee3320cc4
Sudarshansp <sudarshansp123@gmail.com>
2025-01-01 17:57:28 +0530

    Time: 1089 ms (10.32%) | Memory: 44.8 MB (95.32%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,36 +1,53 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> hs = new HashSet<>(wordList);
-
-        Queue<String> q = new LinkedList<String>();
+        Stack<Integer> counterSt = new Stack<>();
+        Queue<String> q = new LinkedList<>();
+        HashSet<String> hs = new HashSet<>();
         q.offer(beginWord);
-        int ans = 1;
+        counterSt.push(1);
 
         while (!q.isEmpty()) {
-            int n = q.size();
-            for (int i=0; i<n; i++) {
-                StringBuilder s = new StringBuilder(q.poll());
-                for (int j=0; j<s.length(); j++) {
-                    char old = s.charAt(j);
-                    for (int k=0; k<26; k++) {
-                        char t = (char)('a' + k);
-                        s.setCharAt(j, t);
-                        String temp = s.toString();
-                        if (hs.contains(temp)) {
-                            if (temp.equals(endWord)) {
-                                return ans + 1;
-                            }
-                            hs.remove(temp);
-                            q.offer(temp);
-                        }
+            int size = q.size();
+            int counter = counterSt.pop();
+
+            while (size-- > 0) {
+                String s = q.poll();
+
+                if (s.compareTo(endWord) == 0) {
+                    return counter;
+                }
+
+                for (String word: wordList) {
+                    if(mutate(s, word) && !hs.contains(word)) {
+                        hs.add(word);
+                        q.offer(word);
+                        counterSt.push(counter+1);
                     }
-                    s.setCharAt(j, old);
                 }
             }
-            ans++;
         }
 
-
         return 0;
+    }
+
+    public boolean mutate(String s1, String s2) {
+        if (s1.compareTo(s2) == 0) {
+            return false;
+        }
+        int i = 0;
+        int count = 0;
+        while (i<s1.length()) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                count++;
+            }
+
+            if (count > 1) {
+                return false;
+            }
+
+            i++;
+        }
+
+        return true;
     }
 }

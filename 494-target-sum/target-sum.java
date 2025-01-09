42fcc4865066cec97889a60b0d2067da9cbdfd07
Sudarshansp <sudarshansp123@gmail.com>
2025-01-09 13:19:42 +0530

    Time: 7 ms (81.79%) | Memory: 45 MB (23.16%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,9 +1,16 @@
 class Solution {
+    int total = 0;
     public int findTargetSumWays(int[] nums, int target) {
-        return getSum(0, nums, target, 0);
+        int n = nums.length;
+        for (int i=0; i<n; i++) {
+            total += nums[i];
+        }
+        Integer[][] dp = new Integer[n][total*2+1];
+
+        return getSum(0, nums, target, 0, dp);
     }
 
-    public int getSum(int index, int[] nums, int target, int sum) {
+    public int getSum(int index, int[] nums, int target, int sum, Integer[][] dp) {
         if (index == nums.length) {
             if (sum == target) {
                 return 1;
@@ -11,15 +18,13 @@ class Solution {
             return 0;
         }
 
-        int ans = 0;
-        int temp = nums[index];
-        sum += temp;
-        ans += getSum(index+1, nums, target, sum);
-        sum -= temp;
-        temp *= -1;
-        sum += temp;
-        ans += getSum(index+1, nums, target, sum);
+        if (dp[index][sum+total] != null) {
+            return dp[index][sum+total];
+        }
+        
+        int add = getSum(index+1, nums, target, sum + nums[index], dp);
+        int subtract = getSum(index+1, nums, target, sum-nums[index], dp);
 
-        return ans;
+        return dp[index][sum+total] = add+subtract;
     }
 }

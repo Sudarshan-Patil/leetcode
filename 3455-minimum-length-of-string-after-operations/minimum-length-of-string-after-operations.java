3089d110095ace121511d85bcc0473b0af19d707
Sudarshansp <sudarshansp123@gmail.com>
2025-01-13 15:22:28 +0530

    Time: 9 ms (92.05%) | Memory: 46.7 MB (24.50%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minimumLength(String s) {
+        int[] arr = new int[26];
+        for (int i=0; i<s.length(); i++) {
+            arr[s.charAt(i)-'a']++;
+        }
+
+        int ans = 0;
+        for (int i=0; i<26; i++) {
+            if (arr[i] >= 3) {
+                if (arr[i]%2 == 0) {
+                    ans += 2;
+                } else {
+                    ans += 1;
+                }
+            } else {
+                ans += arr[i];
+            }
+        }
+        
+        return ans;
+    }
+
+    public void display(int[] arr) {
+        int n = arr.length;
+        for (int i=0; i<n; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        return;
+    }
+
+    public void display1(char[] arr) {
+        int n = arr.length;
+        for (int i=0; i<n; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        return;
+    }
+}

b99da6d348ee8c0e5e43993888ae3848b9b3f7b6
Sudarshansp <sudarshansp123@gmail.com>
2025-04-25 18:35:00 +0530

    Time: 7 ms (89.79%) | Memory: 45.3 MB (33.49%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,19 +1,37 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
-        int totalDistinct = (int) Arrays.stream(nums).distinct().count();
-        Map<Integer, Integer> map = new HashMap<>();
-        int res = 0, left = 0;
+        Map<Integer, Integer> hm = new HashMap<>();
 
-        for (int right = 0; right < nums.length; right++) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            while (map.size() == totalDistinct) {
-                res += nums.length - right;
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0)
-                    map.remove(nums[left]);
+        int left = 0;
+        int right = 0;
+        int n = nums.length;
+
+        HashSet<Integer> hs = new HashSet<>();
+
+        for (int i=0; i<n; i++) {
+            hs.add(nums[i]);
+        }
+        int res = 0;
+        int size = hs.size();
+
+        for (right=0; right<n; right++) {
+            if (hm.containsKey(nums[right])) {
+                hm.put(nums[right], hm.get(nums[right])+1);
+            } else {
+                hm.put(nums[right], 1);
+            }
+
+            while (hm.size() == size) {
+                res += n-right;
+
+                hm.put(nums[left], hm.get(nums[left])-1);
+                if (hm.get(nums[left]) == 0) {
+                    hm.remove(nums[left]);
+                }
                 left++;
             }
         }
-        return res;
+
+        return res; 
     }
 }

6ac53cfb4299982ad0740bbf1151d4d014023dd9
Sudarshansp <sudarshansp123@gmail.com>
2025-05-11 17:55:01 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (7.05%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,14 +1,8 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         int count = 0;
-        for (int i=0; i<arr.length; i++) {
-            if (arr[i]%2 != 0) {
-                count++;
-            } else {
-                count = 0;
-            }
-
-            if (count == 3) {
+        for (int i=1; i<arr.length-1; i++) {
+            if (arr[i]%2 != 0 && arr[i-1]%2 != 0 && arr[i+1]%2 != 0) {
                 return true;
             }
         }

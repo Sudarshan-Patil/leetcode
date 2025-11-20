8d21bf74f2b75930bed7bcbf3441440155ed795b
Sudarshansp <sudarshansp123@gmail.com>
2025-11-20 19:42:29 +0530

    Time: 9 ms (93.20%) | Memory: 47.6 MB (19.05%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
@@ -1,24 +1,32 @@
 class Solution {
     public int intersectionSizeTwo(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> 
-            a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]
-        );
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[1] == b[1]) {
+                return b[0] - a[0];
+            }
+            return a[1] - b[1];
+        });
 
         int ans = 0;
         int a = -1, b = -1;
 
         for (int[] it : intervals) {
             int l = it[0], r = it[1];
+            boolean hasa = a >= l && a <= r;
+            boolean hasb = b >= l && b <= r;
 
-            if (l > b) {
-                a = r - 1;
-                b = r;
-                ans += 2;
-            } else if (l > a) {
+            if (hasa && hasb) {
+                continue;
+            } else if (hasb) {
                 a = b;
                 b = r;
                 ans += 1;
+            } else {
+                a = r-1;
+                b = r;
+                ans += 2;
             }
+
         }
 
         return ans;

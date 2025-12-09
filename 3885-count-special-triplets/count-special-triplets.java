d814a78924b549ea9f0a57bc4b3a2896bb942b26
Sudarshansp <sudarshansp123@gmail.com>
2025-12-10 01:43:35 +0530

    Time: 274 ms (25.20%) | Memory: 194.6 MB (15.75%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.java
+++ 3885-count-special-triplets/count-special-triplets.java
@@ -1,31 +1,33 @@
 class Solution {
-    int mod = 1000000007;
     public int specialTriplets(int[] nums) {
+        int mod = 1000000007;
+        long ans = 0;
+        HashMap<Integer, Integer> left = new HashMap<>();
+        HashMap<Integer, Integer> right = new HashMap<>();
         int n = nums.length;
-        Map<Integer, Integer> mapLeft = new HashMap<>();
-        Map<Integer, Integer> mapRight = new HashMap<>();
-        long cnt = 0;
-        for(int i=2; i<n; i++){
-            mapRight.put(nums[i], mapRight.getOrDefault(nums[i], 0)+1);
+        for (int i=2; i<n; i++) {
+            right.put(nums[i], right.getOrDefault(nums[i], 0)+1);
         }
-        mapLeft.put(nums[0], 1);
-        int i=1;
-        while(i<nums.length-1){
-            int req = 2 * nums[i];
-            if(mapLeft.containsKey(req) && mapRight.containsKey(req)){
-                int n1 = mapLeft.get(req);
-                int n2 = mapRight.get(req);
-                cnt = (cnt + ((long)n1 * (long)n2) % mod) % mod;
-            } 
-            mapLeft.put(nums[i], mapLeft.getOrDefault(nums[i], 0)+1);
-            if(i+1 < nums.length){
-                mapRight.put(nums[i+1], mapRight.get(nums[i+1])-1);
-                if(mapRight.get(nums[i+1]) == 0){
-                    mapRight.remove(nums[i+1]);
-                }
+
+        left.put(nums[0], 1);
+
+        for (int i=1; i<n-1; i++) {
+            int temp = nums[i] * 2;
+            if (left.containsKey(temp) && right.containsKey(temp)) {
+                int n1 = left.get(temp);
+                int n2 = right.get(temp);
+
+                ans = (ans%mod + ((long)n1 * (long)n2)%mod)%mod;
             }
-            i++;
+
+            left.put(nums[i], left.getOrDefault(nums[i], 0) + 1);
+            
+            right.put(nums[i+1], right.get(nums[i+1])-1);
+            if (right.get(nums[i+1]) == 0) {
+                right.remove(nums[i+1]);
+            }
         }
-        return (int)cnt;
+
+        return (int)ans;
     }
 }

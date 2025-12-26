f328a9c76adf7ec779e42ec37a506ed7c5f4bcf5
Sudarshansp <sudarshansp123@gmail.com>
2025-12-26 20:38:35 +0530

    Time: 5 ms (99.83%) | Memory: 47.1 MB (20.69%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,48 +1,20 @@
 class Solution {
     public int bestClosingTime(String customers) {
-        int n = customers.length();
-
-        int[] t1 = new int[n];
-        int[] t2 = new int[n];
-        t1[0] = customers.charAt(0) == 'Y' ? 1 : 0;
-        t2[0] = customers.charAt(0) == 'N' ? 1 : 0;
-        for (int i=1; i<n; i++) {
-            t1[i] = t1[i-1] + (customers.charAt(i) == 'Y' ? 1 : 0);
-            t2[i] = t2[i-1] + (customers.charAt(i) == 'N' ? 1 : 0);
-        }
-        // display(n, t1);
-        // display(n, t2);
-        int ans = Integer.MAX_VALUE;
-        int index = 0;
-
-        for (int i=0; i<n; i++) {
-            if (i == 0) {
-                if (ans > t1[n-1]) {
-                    ans = t1[n-1];
-                    index = i;
+        byte[] cs = customers.getBytes(java.nio.charset.Charset.forName("ISO-8859-1"));
+        int bestTime = -1;
+        int customersLeft = 0;
+        for (int i = 0; i < cs.length; i++) {
+            if (cs[i] == 89) {
+                customersLeft++;
+                if (customersLeft > 0) {
+                    bestTime = i;
+                    customersLeft = 0;
                 }
             } else {
-                int temp = (t1[n-1]-t1[i-1]) + t2[i-1];
-                if (ans > temp) {
-                    ans = temp;
-                    index = i;
-                }
-                // System.out.println(i + " : " + ans + " : " + (t1[n-1]-t1[i-1]) + " : " + t2[i]);
+                customersLeft--;
             }
         }
 
-        if (ans > t2[n-1]) {
-            return n;
-        }
-
-        return index;
-    }
-
-    public void display(int n, int[] a) {
-        for (int i=0; i<n; i++) {
-            System.out.print(a[i] + " ");
-        }
-        System.out.println();
-        return;
+        return bestTime+1;
     }
 }

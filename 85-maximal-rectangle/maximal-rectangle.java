7545ae595c2a034182a28279e24643df5a66967c
Sudarshansp <sudarshansp123@gmail.com>
2026-01-11 05:46:14 +0100

    Time: 12 ms (79.09%) | Memory: 50.4 MB (11.45%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,31 +1,52 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int[][] ans = new int[matrix.length][matrix[0].length];
-        for (int i=0; i<matrix.length; i++) {
-            int sum = 0;
-            for (int j=0; j<matrix[0].length; j++) {
-                if (matrix[i][j] != '0') {
-                    sum += matrix[i][j]-'0';
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int ans = 0;
+
+        int[][] data = new int[row][col];
+
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (matrix[i][j] == '0') {
+                    data[i][j] = 0;
                 } else {
-                    sum = 0;
-                }
-                ans[i][j] = sum;
-            }
-        }
-
-        int res = 0;
-        for (int i=0; i<matrix.length; i++) {
-            for (int j=0; j<matrix[0].length; j++) {
-                int k=i;
-                int min = ans[k][j];
-                while (k>=0 && ans[k][j] > 0) {
-                    min = Math.min(min, ans[k][j]);
-                    res = Math.max(res, min * (i-k+1));
-                    k--;
+                    if (j>0) {
+                        data[i][j] = 1 + data[i][j-1];
+                    } else {
+                        data[i][j] = 1;
+                    }
                 }
             }
         }
 
-        return res;
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                int k = data[i][j];
+                int a = i;
+                while (a>=0 && data[a][j] > 0) {
+                    k = Math.min(k, data[a][j]);
+                    ans = Math.max(ans, (i-a+1)*k);
+                    a--;
+                }
+// System.out.println(a);
+                
+            }
+        }
+
+        // display(data);
+
+        return ans;
+    }
+
+    public void display(int[][] m) {
+        for (int i=0; i<m.length; i++) {
+            for (int j=0; j<m[0].length; j++) {
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return;
     }
 }

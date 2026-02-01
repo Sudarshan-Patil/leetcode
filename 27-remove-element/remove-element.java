672ebb5c1c2c43b1055f1d0803153c47ded52d91
Sudarshansp <sudarshansp123@gmail.com>
2026-02-01 10:53:57 +0100

    Time: 0 ms (100.00%) | Memory: 43.4 MB (73.73%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -3,14 +3,11 @@ class Solution {
         int i=0;
         int j=0;
         while (i<nums.length) {
-            if (nums[i] == val) {
-                i++;
-                continue;
-            } else {
+            if (nums[i] != val) {
                 nums[j] = nums[i];
-                i++;
                 j++;
             }
+            i++;
         }
 
         return j;

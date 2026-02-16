2dc9a37ff3ad0ae99f939ab6515d64f4e1db6193
Sudarshansp <sudarshansp123@gmail.com>
2026-02-16 17:08:24 +0530

    Time: 3 ms (77.28%) | Memory: 48.1 MB (72.56%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -7,7 +7,7 @@ class Solution {
             for (int c = 0; c <= r; c++) {
                 if (dp[r][c] > 1) {
                     double overflow = (dp[r][c] - 1) / 2.0;
-                    dp[r][c] = 1;
+                    // dp[r][c] = 1;
                     dp[r + 1][c] += overflow;
                     dp[r + 1][c + 1] += overflow;
                 }
